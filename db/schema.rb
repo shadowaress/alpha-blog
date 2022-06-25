6b9a9c5fa58917246491ee72171e0be1b204b706
Steven Giguere <sgiguere87@gmail.com>
2022-06-24 20:29:47 -0400

    added statement on some files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_24_023115) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_24_024215) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
